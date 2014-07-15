5743860337f76f3123b268d901d14d69ae903191
Chunpeng Huo <huochunpeng@gmail.com>
2014-07-15 23:13:29 +1000

    update test.

--- spec/bindingHandlers.browserspec.js
+++ spec/bindingHandlers.browserspec.js
@@ -16,7 +16,7 @@
       ko.cleanNode($('#testNode')[0]);
       $('#testNode').remove();
       $('#ui-datepicker-div').remove();
-    })
+    });
 
     it("bind date object to an input", function() {
       var d = ko.observable();
@@ -25,6 +25,10 @@
       expect(node.val()).toBe('');
       d(new Date(2014, 2, 5));
       expect(node.val()).toBe('2014-03-05');
+      $('#testNode input').datepicker("setDate", new Date(2013, 7, 9)).trigger("change");
+      expect(d()).toEqual(new Date(2013, 7, 9));
+      $('#testNode input').val('2014-08-09').trigger("change");
+      expect(d()).toEqual(new Date(2014, 7, 9));
     });
   });
 
